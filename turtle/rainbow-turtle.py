8fc7674737961c78f8dd1a5a1c730978fb572d6c
Vanessa <vxnxssx.wxtzxl@gmail.com>
2024-03-06 14:23:07 +0100

    Create rainbow-turtle.py

---
+++ turtle/rainbow-turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.bgcolor('black')
+t = turtle.Turtle()
+colors = ['white','red','orange','yellow','green','blue','purple']  
+
+t.speed(0)
+
+for number in range(1600): 
+
+    t.forward(number+1)
+    t.right(89)
+    t.pencolor(colors[number % len(colors)])
+
+    print(f"Schleifenindex: {number}, Farbe: {t.pencolor()}")
+
+turtle.done()
